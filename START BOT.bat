579e8fe714250bd937f3d46fced588658a63c66e
JVSTV-2108 <60276119+JVSTV-2108@users.noreply.github.com>
2020-03-14 18:49:34 +0100

    Add files via upload

---
+++ START BOT.bat
@@ -0,0 +1,11 @@
+echo off
+cls
+color 4
+echo For Help: Youtube: Orix. (https://www.youtube.com/channel/UCJ1A0zH-WgyQe3JzfQS3HTg)
+echo ------------------------------------------
+pause
+cls
+color  1
+
+py fortnite.py
+cmd /k
